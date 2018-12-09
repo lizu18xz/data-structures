88d21dbb296a7d771aeb3f85b7297d711a355b16
lizu <535733495@qq.com>
2018-12-09 21:59:47 +0800

    二分搜索树  遍历的实现

--- src/main/java/com/fayayo/ch6/BST.java
+++ src/main/java/com/fayayo/ch6/BST.java
@@ -1,5 +1,9 @@
 package com.fayayo.ch6;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author dalizu on 2018/12/6.
  * @version v1.0
@@ -38,13 +42,173 @@ public class BST <E extends Comparable<E>>{
 
     //添加元素
     public void add(E e){
+        root=add(root,e);
+    }
+
+    //向以node为根的二分搜索树中添加元素E,递归算法
+    private Node add(Node node,E e){
+
+        //终止条件
+        if(node == null){
+            size++;
+            return new Node(e);
+        }
+
+        //递归操作
+        if(e.compareTo(node.e)<0){
+            node.left=add(node.left,e);
+        }else if(e.compareTo(node.e)>0){
+            node.right=add(node.right,e);
+        }
+        return node;
+
+    }
+
+
+    public boolean contains(E e){
+
+        return contains(root,e);
+
+    }
+    //以node为根的二分搜索树是否包含元素e
+    private boolean contains(Node node,E e){
+        if(node ==null){
+            return false;
+        }
+        if(e.compareTo(node.e)==0){
+            return true;
+        }else if(e.compareTo(node.e)<0){
+            return contains(node.left,e);
+        }else {
+            return contains(node.right,e);
+        }
+    }
+
+
+    //前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    //前序遍历以node为根的二分搜索树
+    private void preOrder(Node node){
+        if(node==null){
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    //前序遍历 非递归
+    public void preOrderNR(){
+        Stack <Node>stack=new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            Node cur=stack.pop();//栈顶取出元素
+            System.out.println(cur.e);
+            if(cur.right!=null){
+                stack.push(cur.right);
+            }
+
+            if(cur.left!=null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
+
+
+    //中序遍历(结果是顺序的)
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(Node node){
+
+        if(node==null){
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+
+    }
+
+    //后序遍历(结果是顺序的)
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    public void postOrder(Node node){
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(node.e);
+    }
+
+
+    //二分搜索树 层序遍历  广度优先
+    public void levelOrder(){
+
+        Queue<Node>queue=new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+
+            Node cur= queue.remove();
+            System.out.println(cur.e);
+
+            if(cur.left!=null){
+                queue.add(cur.left);
+            }
+            if(cur.right!=null){
+                queue.add(cur.right);
+            }
+
+        }
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder res=new StringBuilder();
+        generateBSTString(root,0,res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+
+        if(node==null){
+            res.append(generateDepthString(depth)+"null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth)+node.e+"\n");
+        generateBSTString(node.left,depth+1,res);
+        generateBSTString(node.right,depth+1,res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res=new StringBuilder();
+        for (int i=0;i<depth;i++){
+            res.append(" _ _");
+        }
+        return res.toString();
+    }
+
+
+
+    /*   //添加元素
+    public void add(E e){
         if(root == null){
             root = new Node(e);
             size++;
         }else {
             add(root,e);
         }
-
     }
 
     //向以node为根的二分搜索树中添加元素E,递归算法
@@ -69,9 +233,7 @@ public class BST <E extends Comparable<E>>{
         }else {
             add(node.right,e);
         }
-
-
-    }
+    }*/
 
 
 
--- src/main/java/com/fayayo/ch6/Main.java
+++ src/main/java/com/fayayo/ch6/Main.java
@@ -6,4 +6,22 @@ package com.fayayo.ch6;
  * @desc
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        BST<Integer>bst=new BST<>();
+
+        int num[]={5,3,6,8,4,2};
+
+        for(int n:num){
+               bst.add(n);
+        }
+        /*bst.preOrder();
+
+        bst.preOrderNR();
+        System.out.println();
+        System.out.println(bst);*/
+
+        bst.levelOrder();
+    }
 }
